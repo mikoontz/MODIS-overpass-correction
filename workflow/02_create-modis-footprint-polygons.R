79e61fa1d69a1442881aae3e7ff3dd3971a03b32
Michael Koontz <mikoontz@gmail.com>
2020-02-25 13:33:12 -0700

    implement a rotated projection approach instead of a 'build a polygon from separate linestrings' approach

--- workflow/02_create-modis-footprint-polygons.R
+++ workflow/02_create-modis-footprint-polygons.R
@@ -15,35 +15,6 @@ library(geosphere)
 plan(multiprocess)
 
 
-# dateline split ----------------------------------------------------------
-
-.breakAtDateLine <- function(x) {
-  r <- range(x[,1]) 
-  r <- r[2] - r[1]
-  # if (r > 200) {
-    dif <- abs(x[-nrow(x),1] - x[-1,1])
-    tr <- which(dif==max(dif))
-    x1 <- x[1:tr, ,drop=FALSE]
-    x2 <- x[(tr+1):nrow(x), ,drop=FALSE]
-    if (x1[tr,1] < 0) { 
-      x1[tr,1] <- -180 
-      x2[1,1] <- 180
-    } else { 
-      x1[tr,1] <- 180 
-      x2[1,1] <- -180 
-    }
-    if (nrow(x1) <= 1) {
-      res <- x2
-    } else if (nrow(x2) <= 1) {
-      res <- x1
-    } else {
-      res <- list(x1, x2)
-    }
-    return(res)
-  # } 
-  # return(x)
-}
-
 # global variables --------------------------------------------------------
 
 global_extent <- 
@@ -56,143 +27,185 @@ global_extent <-
 
 # function to write polygons (a slow step) to disk ------------------------------------
 
+get_MODIS_granule_footprints <- function(satellite, path, year, month, day, yday, GranuleID, StartDateTime, ArchiveSet, OrbitNumber, DayNightFlag, EastBoundingCoord, NorthBoundingCoord, SouthBoundingCoord, WestBoundingCoord, GRingLongitude1, GRingLongitude2, GRingLongitude3, GRingLongitude4, GRingLatitude1, GRingLatitude2, GRingLatitude3, GRingLatitude4) {
+  
+  pt1 <- c(GRingLongitude1, GRingLatitude1)
+  pt2 <- c(GRingLongitude2, GRingLatitude2)
+  pt3 <- c(GRingLongitude3, GRingLatitude3)
+  pt4 <- c(GRingLongitude4, GRingLatitude4)
+  
+  linestring_resolution <- units::set_units(10, km)
+  # If the footprint overlaps one of the poles, then the WestBoundingCoord
+  # and the EastBoundingCoord will be -180 and 180, respectively
+  # This requires a special accomodation to ensure the right polygon is made
+  if(WestBoundingCoord == -180 | EastBoundingCoord == 180) {
+    
+    mat <- st_sfc(st_multipoint(matrix(rbind(pt1, pt2, pt3, pt4, pt1), ncol = 2)), crs = 4326)
+    
+    # Create a single linestring that spans the -180 to 180 global extent
+    # which will be used to split a polygon of the global extent
+    blade <- 
+      mat %>% 
+      st_cast("LINESTRING") %>%
+      st_segmentize(dfMaxLength = linestring_resolution) %>% 
+      st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=180")) %>% 
+      st_cast("POINT") %>% 
+      sf::st_coordinates()
+    
+    blade <- blade[order(blade[, "X"]), ]
+    
+    blade <-
+      blade %>% 
+      st_linestring() %>% 
+      st_sfc(crs = 4326)
+    
+    split_globe <- lwgeom::st_split(x = global_extent, y = blade)
+    
+    suppressWarnings({
+      polys <- 
+        st_collection_extract(x = split_globe, type = "POLYGON") %>% 
+        dplyr::mutate(centroid_lat = st_coordinates(st_centroid(.))[, 2])
+    })
+    
+    if(SouthBoundingCoord > 0) {
+      footprint <- 
+        polys %>% 
+        filter(centroid_lat == max(centroid_lat)) %>% 
+        dplyr::select(-centroid_lat)
+      
+    } else {
+      footprint <- 
+        polys %>% 
+        filter(centroid_lat == min(centroid_lat)) %>% 
+        dplyr::select(-centroid_lat)
+      
+    }
+  } else { 
+    
+    # We know when the footprint crosses the dateline because the
+    # WestBoundingCoord is actually greater than the EastBoundingCoord
+    # First, we'll deal with the simple case where the footprint doesn't
+    # cross the dateline
+    
+    if (WestBoundingCoord < EastBoundingCoord) {
+      
+      mat <- st_sfc(st_multipoint(matrix(rbind(pt1, pt2, pt3, pt4, pt1), ncol = 2)), crs = 4326)
+      
+      footprint <- 
+        mat %>% 
+        st_cast("LINESTRING") %>%
+        st_segmentize(dfMaxLength = linestring_resolution) %>% 
+        st_cast("POLYGON") %>% 
+        st_sfc(crs = 4326) %>% 
+        st_sf()
+      
+    } else 
+      # The condition when the footprint crosses the dateline
+      # Rotate the frame of reference so the dateline is now at 0 degrees
+      # Build the polygon (that now won't cross the 'dateline' of the 
+      # modified projection)
+      # Split the geometry at the 0 meridian in the rotated projection
+      # (which is the dateline in the original projection)
+      # Translate the split polygons to their appropriate places
+      if (WestBoundingCoord >= EastBoundingCoord) {
+       
+        offset <- 180
+        
+        pt1 <- pt1 + c(offset, 0)
+        pt2 <- pt2 + c(offset, 0)
+        pt3 <- pt3 + c(offset, 0)
+        pt4 <- pt4 + c(offset, 0)
+        
+        
+        pt1 <- pt1 + c(ifelse(pt1[1] > 180, yes = -360, no = 0), 0)
+        pt2 <- pt2 + c(ifelse(pt2[1] > 180, yes = -360, no = 0), 0)
+        pt3 <- pt3 + c(ifelse(pt3[1] > 180, yes = -360, no = 0), 0)
+        pt4 <- pt4 + c(ifelse(pt4[1] > 180, yes = -360, no = 0), 0)
+        
+        mat <- sf::st_sfc(st_multipoint(matrix(rbind(pt1, pt2, pt3, pt4, pt1), ncol = 2)), crs = 4326)
+        
+        footprint <- 
+          mat %>% 
+          sf::st_cast("LINESTRING") %>%
+          sf::st_segmentize(dfMaxLength = linestring_resolution) %>% 
+          sf::st_cast("POLYGON") %>% 
+          sf::st_sfc(crs = 4326) %>% 
+          sf::st_sf()
+        
+        blade <- 
+          sf::st_linestring(x = matrix(rbind(c(0, 90), c(0, -90)), ncol = 2)) %>% 
+          sf::st_sfc(crs = 4326)
+        
+        split_poly <- lwgeom::st_split(x = footprint, y = blade)
+        
+        suppressWarnings({
+          polys <- 
+            sf::st_collection_extract(x = split_poly, type = "POLYGON") %>% 
+            dplyr::mutate(centroid_lon = st_coordinates(st_centroid(.))[, 1])
+        })
+        
+        east_poly <- 
+          polys %>% 
+          dplyr::filter(centroid_lon < 0) %>% 
+          dplyr::summarize() %>% 
+          sf::st_geometry()
+        
+        east_poly <- east_poly + sf::st_sfc(st_point(c(offset, 0)))
+        east_poly <- 
+          east_poly %>% 
+          sf::st_set_crs(4326)
+        
+        west_poly <-
+          polys %>% 
+          dplyr::filter(centroid_lon >= 0) %>% 
+          dplyr::summarize() %>% 
+          sf::st_geometry()
+        
+        west_poly <- west_poly + sf::st_sfc(st_point(c(-offset, 0)))
+        west_poly <- 
+          west_poly %>% 
+          sf::st_set_crs(4326)
+          
+        footprint <- 
+          lapply(list(east_poly, west_poly), sf::st_as_sf) %>% 
+          do.call("rbind", .) %>% 
+          dplyr::rename(geometry = x) %>% 
+          dplyr::summarize()
+
+      } 
+  }
+  
+  # rebuild the sf object row by row
+  # cast everything to multipolygon so we can combine the resulting 
+  # list elements with data.table::rbindlist()
+  obj <- 
+    tibble(satellite, path, year, month, day, yday, GranuleID, StartDateTime, ArchiveSet, OrbitNumber, DayNightFlag, EastBoundingCoord, NorthBoundingCoord, SouthBoundingCoord, WestBoundingCoord, GRingLongitude1, GRingLongitude2, GRingLongitude3, GRingLongitude4, GRingLatitude1, GRingLatitude2, GRingLatitude3, GRingLatitude4, geometry = footprint$geometry) %>% 
+    st_sf() %>% 
+    st_cast("MULTIPOLYGON") 
+  
+  return(obj)
+  
+}
+
 get_MODIS_footprints <- function(geoMeta) {
   
   geoMeta <- tidyr::unnest(geoMeta, cols = geoMeta)
   
   footprints <-
-    pmap(geoMeta, .f = function(satellite, path, year, month, day, yday, GranuleID, StartDateTime, ArchiveSet, OrbitNumber, DayNightFlag, EastBoundingCoord, NorthBoundingCoord, SouthBoundingCoord, WestBoundingCoord, GRingLongitude1, GRingLongitude2, GRingLongitude3, GRingLongitude4, GRingLatitude1, GRingLatitude2, GRingLatitude3, GRingLatitude4) {
-      
-      pt1 <- c(GRingLongitude1, GRingLatitude1)
-      pt2 <- c(GRingLongitude2, GRingLatitude2)
-      pt3 <- c(GRingLongitude3, GRingLatitude3)
-      pt4 <- c(GRingLongitude4, GRingLatitude4)
-      
-      # We know when the footprint crosses the dateline because the
-      # WestBoundingCoord is actually greater than the EastBoundingCoord
-      if (WestBoundingCoord > EastBoundingCoord) {
-        
-        line1 <- 
-          st_linestring(matrix(rbind(pt1, pt2), ncol = 2)) %>% 
-          st_sfc(crs = 4326) %>% 
-          st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=100")) %>% 
-          st_segmentize(dfMaxLength = units::set_units(10, km)) %>% 
-          st_wrap_dateline()
-        
-        line2 <- 
-          st_linestring(matrix(rbind(pt2, pt3), ncol = 2)) %>% 
-          st_sfc(crs = 4326) %>% 
-          st_segmentize(dfMaxLength = units::set_units(10, km))
-
-        line3 <- 
-          st_sfc(st_linestring(rbind(pt3, pt4)), crs = 4326) %>% 
-          st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=190"))   %>%
-          st_segmentize(units::set_units(10, km)) %>% 
-          st_wrap_dateline()
-
-        line4 <- 
-          st_linestring(matrix(rbind(pt4, pt1), ncol = 2)) %>% 
-          st_sfc(crs = 4326)
-        
-        footprint_line <- do.call("rbind", lapply(list(line1, line2, line3, line4), st_as_sf))
-        
-      } else {
-        
-        mat <- st_sfc(st_multipoint(matrix(rbind(pt1, pt2, pt3, pt4, pt1), ncol = 2)), crs = 4326)
-        
-        # 
-        # line1 <- geosphere::gcIntermediate(pt1, pt2, breakAtDateLine = TRUE, sp = TRUE) %>% st_as_sf()
-        # 
-        # line2 <- geosphere::gcIntermediate(pt2, pt3, breakAtDateLine = TRUE, sp = TRUE) %>% st_as_sf()
-        # line3 <- geosphere::gcIntermediate(pt3, pt4)
-        # test1 <- .breakAtDateLine(line3)[[1]] %>% as.data.frame() %>% st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>% summarize() %>% st_cast("LINESTRING")
-        # 
-        # test2 <- .breakAtDateLine(line3)[[2]] %>% as.data.frame() %>% st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>% summarize() %>% st_cast("LINESTRING")
-        # 
-        # line3 <- do.call("rbind", list(test1, test2))
-        # line4 <- geosphere::gcIntermediate(pt4, pt1, breakAtDateLine = TRUE, sp = TRUE) %>% st_as_sf()
-        
-        plot(line1, axes = TRUE)
-        plot(line2, add = TRUE)
-        plot(line3, add = TRUE)
-        plot(line4, add = TRUE)
-        
-        footprint_line <-
-          mat %>% 
-          st_cast("MULTILINESTRING") %>%
-          st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=190")) %>%
-          st_segmentize(dfMaxLength = units::set_units(10, km)) %>% 
-          st_wrap_dateline()
-      }
-
-      if(WestBoundingCoord == -180 | EastBoundingCoord == 180) {
-        
-        blade <- 
-          footprint_line %>% 
-          st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=180")) %>% 
-          st_cast("POINT") %>% 
-          sf::st_coordinates()
-        
-        blade <- blade[order(blade[, "X"]), ]
-        
-        blade <-
-          blade %>% 
-          st_linestring() %>% 
-          st_sfc(crs = 4326)
-        
-        split_globe <- lwgeom::st_split(x = global_extent, y = blade)
-        
-        suppressWarnings({
-          polys <- 
-            st_collection_extract(x = split_globe, type = "POLYGON") %>% 
-            dplyr::mutate(centroid_lat = st_coordinates(st_centroid(.))[, 2])
-        })
-        
-        if(SouthBoundingCoord > 0) {
-          footprint <- 
-            polys %>% 
-            filter(centroid_lat == max(centroid_lat)) %>% 
-            dplyr::select(-centroid_lat)
-          
-        } else {
-          footprint <- 
-            polys %>% 
-            filter(centroid_lat == min(centroid_lat)) %>% 
-            dplyr::select(-centroid_lat)
-          
-        }
-      } else {
-        footprint <- 
-          footprint_line %>% 
-          st_cast("POLYGON") %>% 
-          st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=180")) %>% 
-          st_sfc(crs = 4326) %>% 
-          st_sf(geometry = .)
-        
-      }
-      
-      # rebuild the sf object row by row
-      # cast everything to multipolygon so we can combine the resulting 
-      # list elements with data.table::rbindlist()
-      obj <- 
-        tibble(satellite, path, year, month, day, yday, GranuleID, StartDateTime, ArchiveSet, OrbitNumber, DayNightFlag, EastBoundingCoord, NorthBoundingCoord, SouthBoundingCoord, WestBoundingCoord, GRingLongitude1, GRingLongitude2, GRingLongitude3, GRingLongitude4, GRingLatitude1, GRingLatitude2, GRingLatitude3, GRingLatitude4, geometry = footprint$geometry) %>% 
-        st_sf() %>% 
-        st_cast("MULTIPOLYGON") 
-      
-      return(obj)
-      
-    })
+    pmap(geoMeta, .f = get_MODIS_granule_footprints)
   
   footprints <- do.call(what = "rbind", args = footprints)
   
   this_year <- unique(footprints$year)
   this_month <- unique(footprints$month)
-  this_file <- paste0(this_year, "-", this_month, "_5-minute-footprints.gpkg")
+  this_satellite <- unique(footprints$satellite)
+  
+  this_file <- paste0(this_year, "-", this_month, "_", this_satellite, "_5-minute-footprints.gpkg")
   
   this_path <- file.path("data", "data_output", "MODIS-footprints", this_file)
-  
+
   sf::st_write(obj = footprints, dsn = this_path, delete_dsn = TRUE)
-  
+
   system2(command = "aws", args = paste0('s3 cp ', this_path, ' s3://earthlab-mkoontz/MODIS-footprints/', this_file))
   
   return(footprints)
@@ -236,6 +249,8 @@ aqua <-
     
   }))
 
+
+# get terra geometa61 -----------------------------------------------------
 terra <- 
   list.files("data/data_output/MODIS-geoMeta61/TERRA", recursive = TRUE, full.names = TRUE) %>% 
   tibble(satellite = "Terra",
@@ -273,72 +288,14 @@ terra <-
 
 geoMeta_df <- rbind(aqua, terra)
 
-geoMeta <- geoMeta_df %>% filter(path == "data/data_output/MODIS-geoMeta61/AQUA/2005/MYD03_2005-06-02.txt")
+# test small snippets of geoMeta for polygon creation ---------------------
 
-geoMeta <- tidyr::unnest(geoMeta, cols = geoMeta)
+geoMeta_target <- 
+  geoMeta_df %>% 
+  dplyr::filter(path == "data/data_output/MODIS-geoMeta61/AQUA/2005/MYD03_2005-06-02.txt")
 
-# test <- geoMeta %>% filter(GranuleID %in% c("MYD03.A2005153.0030.061.2018022195730.hdf", 
-#                                             "MYD03.A2005153.0035.061.2018022195452.hdf"))
-# 
-# geoMeta <- geoMeta %>% filter(GranuleID == "MYD03.A2005153.0030.061.2018022195730.hdf")
-
-test <-
-  pmap(geoMeta[8:10, ], .f = function(satellite, path, year, month, day, yday, GranuleID, StartDateTime, ArchiveSet, OrbitNumber, DayNightFlag, EastBoundingCoord, NorthBoundingCoord, SouthBoundingCoord, WestBoundingCoord, GRingLongitude1, GRingLongitude2, GRingLongitude3, GRingLongitude4, GRingLatitude1, GRingLatitude2, GRingLatitude3, GRingLatitude4) {
-    
-    pt1 <- c(GRingLongitude1, GRingLatitude1)
-    pt2 <- c(GRingLongitude2, GRingLatitude2)
-    pt3 <- c(GRingLongitude3, GRingLatitude3)
-    pt4 <- c(GRingLongitude4, GRingLatitude4)
-    
-    mat <- st_sfc(st_multipoint(matrix(rbind(pt1, pt2, pt3, pt4, pt1), ncol = 2)), crs = 4326) %>% 
-      st_sf() %>% 
-      st_cast("POINT") %>% 
-      rowid_to_column() %>% 
-      dplyr::mutate(rowid = ifelse(rowid == 5, yes = 1, no = rowid))
-    
-    
-  }) %>% do.call("rbind", .)
-
-plot(test, axes = TRUE)
-
-test2 <- 
-  geoMeta %>% 
-  dplyr::mutate(north_pole_overlap = 
-                  NorthBoundingCoord - GRingLatitude1 > 0 |
-                  NorthBoundingCoord - GRingLatitude2 > 0 |
-                  NorthBoundingCoord - GRingLatitude3 > 0 |
-                  NorthBoundingCoord - GRingLatitude4 > 0)
-
-abs(-17.4247071) - abs(c(-34.8902211, -38.665270, -20.383383, -17.3517469))
-
-satellite <- geoMeta$satellite[7]
-path <- geoMeta$path[7]
-year <- geoMeta$year[7]
-month <- geoMeta$month[7]
-day <- geoMeta$day[7]
-yday <- geoMeta$yday[7]
-GranuleID <- geoMeta$GranuleID[7]
-StartDateTime <- geoMeta$StartDateTime[7]
-ArchiveSet <- geoMeta$ArchiveSet[7]
-OrbitNumber <- geoMeta$OrbitNumber[7]
-DayNightFlag <- geoMeta$DayNightFlag[7]
-EastBoundingCoord <- geoMeta$EastBoundingCoord[7]
-NorthBoundingCoord <- geoMeta$NorthBoundingCoord[7]
-SouthBoundingCoord <- geoMeta$SouthBoundingCoord[7]
-WestBoundingCoord <- geoMeta$WestBoundingCoord[7]
-GRingLongitude1 <- geoMeta$GRingLongitude1[7]
-GRingLongitude2 <- geoMeta$GRingLongitude2[7]
-GRingLongitude3 <- geoMeta$GRingLongitude3[7]
-GRingLongitude4 <- geoMeta$GRingLongitude4[7]
-GRingLatitude1 <- geoMeta$GRingLatitude1[7]
-GRingLatitude2 <- geoMeta$GRingLatitude2[7]
-GRingLatitude3 <- geoMeta$GRingLatitude3[7]
-GRingLatitude4 <- geoMeta$GRingLatitude4[7]
-
-
-
-
-
+footprints <- get_MODIS_footprints(geoMeta_target)
+plot(footprints[sample(1:nrow(footprints), size = 20), ] %>% st_geometry(), axes = TRUE)
 
 # Find out what footprints have already been processed and don't redo that work
 already_processed <- 
